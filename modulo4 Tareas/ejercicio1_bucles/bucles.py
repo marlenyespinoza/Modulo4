9a3fd3b6c96f9849fe51b8730339450d6b9becb3
Marleny Espinoza <marlenyespinoza@mail.weber.edu>
2025-09-16 21:25:58 -0600

    Tareas Terminadas

---
+++ modulo4 Tareas/ejercicio1_bucles/bucles.py
@@ -0,0 +1,14 @@
+#TODO 1: Escriba un bucle (for loop) que imprima los números de 0 a 5 (inclusive).
+#Pista: Para que incluya el numero 5
+num = (0, 1, 2, 3, 4, 5)
+for i in num:
+    print(i)
+
+#TODO 2: Escriba un bucle for que imprima la frase "Soy " tres veces
+# Pista: Solo imprimir el texto no cambiarlo
+line = "Soy "
+print(line * 3)
+#TODO 3: Escriba un bucle (for loop) que imprima los números pares del 2 - 10 (inclusivo).
+#Pista: usa range() para contar de dos en dos.
+for i in range(2, 11, 2):
+    print(i)
---
+++ modulo4 Tareas/ejercicio3_arco/codigo3.py
@@ -0,0 +1,12 @@
+# TODO: Usa el código del archivo `codigo2.py` como base para crear una figura diferente.
+# Algunas ideas: un arco, un paralelogramo, una estrella, o cualquier otra figura creativa.
+import turtle
+t = turtle.Turtle()
+t.color("yellow")
+t.pensize(5)
+
+for _ in range(5):
+    t.forward(150)
+    t.right(144)
+
+turtle.done()
